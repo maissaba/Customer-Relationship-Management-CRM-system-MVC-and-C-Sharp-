aa981d539b27c78196b98933071327641827ca84
Maissa BA <Maissa BA@LAPTOP-QV90CJQB>
2022-05-15 14:36:34 +0100

    Ajoutez des fichiers projet.

---
+++ SystémeGestionClients/Models/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystémeGestionClients.Models
+{
+    public struct Adresse
+    {
+        public string rue;
+        public string ville;
+        public string pays;
+        public Adresse(string rue, string ville, string pays)
+        {
+            this.rue = rue;
+            this.ville = ville;
+            this.pays = pays;
+        }
+
+    }
+
+    public enum TypeC
+    {
+        Professionnel,
+        Particulier
+    }
+    
+    public class Client
+    {
+        public string ClientName { get; set; }
+        public TypeC typeC { get; set; }
+        public Adresse adresse  = new Adresse("20, Rue des orangers", "Ezzahra", "Ben Arous");
+
+
+    }
+}
---
+++ SystémeGestionClients/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SystémeGestionClients.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
